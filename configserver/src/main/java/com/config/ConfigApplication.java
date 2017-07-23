0bbcb48ca3230cb5719220ca2677b7d298804a1c
Sherali Obidov <sobidov@spins.com>
2017-07-23 15:41:32 -0500

    config server added

---
+++ configserver/src/main/java/com/config/ConfigApplication.java
@@ -0,0 +1,18 @@
+package com.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author Sherali Obidov.
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+
+}
